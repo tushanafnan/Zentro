226f6103fff12af7c3fc833f13cd1a099e893221
Tushan Afnan <tush.pay@gmail.com>
2023-11-09 16:18:34 +0800

    Firebase Setup

---
+++ js/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCfrvDRFipGktvDS6UAJ1ffCLOpAQOY2uI",
+    authDomain: "zentro-28105.firebaseapp.com",
+    databaseURL: "https://zentro-28105-default-rtdb.firebaseio.com",
+    projectId: "zentro-28105",
+    storageBucket: "zentro-28105.appspot.com",
+    messagingSenderId: "559373828543",
+    appId: "1:559373828543:web:27296494174fc7c2aee10a"
+};
